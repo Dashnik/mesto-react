6f6e8b6b01a9346260015a6f6a9759c94b074268
Alexey Kovalev <Alexey.Kovalev@kofax.com>
2021-01-27 13:35:00 +0300

    Refactor:variable is set like a object for SelectedCard

--- src/components/App.js
+++ src/components/App.js
@@ -1,64 +1,38 @@
+/** Код для десятой проектной работы */
 import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import PopupWithForm from './PopupWithForm';
-import Api from '../utils/Api';
 import ImagePopup from './ImagePopup';
 
 function App() {
-  const [isEditProfilePopupOpen, setProfilePopup] = React.useState(false);
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
   const [isAddPlacePopupOpen, setPlacePopup] = React.useState(false);
   const [isEditAvatarPopupOpen, setAvatarPopup] = React.useState(false);
 
-  const [userName, setUserName] = React.useState('');
-  const [userDescription, setUserDescription] = React.useState('');
-  const [userAvatar, setUserAvatar] = React.useState('');
-  const [cards, setCards] = React.useState([]);
+ 
 
-  const [selectedCard, setImageCard] = React.useState([false]);
+  const [selectedCard, setImageCard] = React.useState({ isOpen:false, name:'', imageSrc:'' });
 
   /**Обработчик событий для открытия карточки */
+  console.log('default value from selectedCard: ' + selectedCard);
 
   const handleCardClick = (imageSrc, cardTitle) => {
+   
     setImageCard({
       ...selectedCard,
       isOpen: true,
       name: cardTitle,
-      imageSrc: imageSrc,
+      imageSrc:imageSrc
     });
+    console.log(selectedCard);
   };
 
-  React.useEffect(() => {
-    Api.getProfileInfo().then((data) => {
-      setUserName(data.name);
-      setUserDescription(data.about);
-      setUserAvatar(data.avatar);
-    })
-    .catch(error=>{
-      console.log(error);
-    });
-  }, []);
-
-  React.useEffect(() => {
-    Api.getInitialCards().then((data) => {
-      setCards(
-        data.map((item) => ({
-          cardID: item._id,
-          imageSrc: item.link,
-          imageAlt: item.name,
-          cardTitle: item.name,
-          cardLikes: item.likes.length,
-        }))
-      );
-    })
-    .catch(error => {
-        console.log(error);
-      });
-  }, []);
+ 
 
   const handleEditProfileClick = () => {
-    setProfilePopup(!isEditProfilePopupOpen);
+    setIsEditProfilePopupOpen(!isEditProfilePopupOpen);
   };
 
   const handleAddPlaceClick = () => {
@@ -70,11 +44,11 @@ function App() {
   };
 
   const closeAllPopups = () => {
-    setProfilePopup(false);
+    setIsEditProfilePopupOpen(false);
     setPlacePopup(false);
     setAvatarPopup(false);
 
-    setImageCard(false);
+    setImageCard({ name:'', link:'' , isOpen: false});
   };
 
   return (
@@ -82,13 +56,9 @@ function App() {
       <Header />
       <Main
         handleCardClick={handleCardClick}
-        allCards={cards}
         onEditProfile={handleEditProfileClick}
         onEditAvatar={handleEditAvatarClick}
         onAddPlace={handleAddPlaceClick}
-        name={userName}
-        description={userDescription}
-        userAvatar={userAvatar}
       />
       <Footer />
       <PopupWithForm
@@ -133,3 +103,145 @@ function App() {
 }
 
 export default App;
+
+
+
+/** Код для 11 проектной работы */
+// import React from 'react';
+// import Header from './Header';
+// import Main from './Main';
+// import Footer from './Footer';
+// import PopupWithForm from './PopupWithForm';
+// import Api from '../utils/api';
+// import ImagePopup from './ImagePopup';
+
+// function App() {
+//   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
+//   const [isAddPlacePopupOpen, setPlacePopup] = React.useState(false);
+//   const [isEditAvatarPopupOpen, setAvatarPopup] = React.useState(false);
+
+//   const [userName, setUserName] = React.useState('');
+//   const [userDescription, setUserDescription] = React.useState('');
+//   const [userAvatar, setUserAvatar] = React.useState('');
+//   const [cards, setCards] = React.useState([]);
+
+//   const [selectedCard, setImageCard] = React.useState({ isOpen:false, name:'', imageSrc:'' });
+
+//   /**Обработчик событий для открытия карточки */
+//   console.log('default value from selectedCard: ' + selectedCard);
+
+//   const handleCardClick = (imageSrc, cardTitle) => {
+   
+//     setImageCard({
+//       ...selectedCard,
+//       isOpen: true,
+//       name: cardTitle,
+//       imageSrc:imageSrc
+//     });
+//     console.log(selectedCard);
+//   };
+
+//   React.useEffect(() => {
+//     Api.getProfileInfo().then((data) => {
+//       setUserName(data.name);
+//       setUserDescription(data.about);
+//       setUserAvatar(data.avatar);
+//     })
+//     .catch(error=>{
+//       console.log(error);
+//     });
+//   }, []);
+
+//   React.useEffect(() => {
+//     Api.getInitialCards().then((data) => {
+//       setCards(
+//         data.map((item) => ({
+//           cardID: item._id,
+//           imageSrc: item.link,
+//           imageAlt: item.name,
+//           cardTitle: item.name,
+//           cardLikes: item.likes.length,
+//         }))
+//       );
+//     })
+//     .catch(error => {
+//         console.log(error);
+//       });
+//   }, []);
+
+//   const handleEditProfileClick = () => {
+//     setIsEditProfilePopupOpen(!isEditProfilePopupOpen);
+//   };
+
+//   const handleAddPlaceClick = () => {
+//     setPlacePopup(!isAddPlacePopupOpen);
+//   };
+
+//   const handleEditAvatarClick = () => {
+//     setAvatarPopup(!isEditAvatarPopupOpen);
+//   };
+
+//   const closeAllPopups = () => {
+//     setIsEditProfilePopupOpen(false);
+//     setPlacePopup(false);
+//     setAvatarPopup(false);
+
+//     setImageCard({ name:'', link:'' , isOpen: false});
+//   };
+
+//   return (
+//     <div className="page">
+//       <Header />
+//       <Main
+//         handleCardClick={handleCardClick}
+//        allCards={cards}
+//         onEditProfile={handleEditProfileClick}
+//         onEditAvatar={handleEditAvatarClick}
+//         onAddPlace={handleAddPlaceClick}
+//         name={userName}
+//         description={userDescription}
+//         userAvatar={userAvatar}
+//       />
+//       <Footer />
+//       <PopupWithForm
+//         isSecondInputActive={true}
+//         onClose={closeAllPopups}
+//         isOpen={isEditProfilePopupOpen ? 'popup_opened' : ''}
+//         name="edit-user-profile"
+//         title="Редактировать профиль"
+//         placeholderName="Имя"
+//         placeholderDescription="Вид деятельности"
+//         submitName="Сохранить"
+//       />
+//       <PopupWithForm
+//         isSecondInputActive={true}
+//         onClose={closeAllPopups}
+//         isOpen={isAddPlacePopupOpen ? 'popup_opened' : ''}
+//         name="new-cards"
+//         title="Новое место"
+//         placeholderName="Название"
+//         placeholderDescription="Ссылка на картинку"
+//         submitName="Создать"
+//       />
+//       <PopupWithForm
+//         isSecondInputActive={false}
+//         onClose={closeAllPopups}
+//         isOpen={isEditAvatarPopupOpen ? 'popup_opened' : ''}
+//         name="editing_photo_profile"
+//         title="Обновить аватар"
+//         placeholderName="Ссылка на аватар"
+//         submitName="Сохранить"
+//       />
+//       <PopupWithForm
+//         isSecondInputActive={false}
+//         onClose={closeAllPopups}
+//         name="removing_card"
+//         title="Вы уверены?"
+//         submitName="Да"
+//       />
+//       <ImagePopup onClose={closeAllPopups} card={selectedCard} />
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,45 @@
+/**Код для 10 проектной работы */
 import React from 'react';
 import Card from './Card';
+import Api from '../utils/api';
 
 function Main(props) {
   function handleClick(imageSrc, cardTitle) {
     props.handleCardClick(imageSrc, cardTitle);
   }
+
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setUserDescription] = React.useState('');
+  const [userAvatar, setUserAvatar] = React.useState('');
+  const [cards, setCards] = React.useState([]);
+
+  React.useEffect(() => {
+    Api.getProfileInfo().then((data) => {
+      setUserName(data.name);
+      setUserDescription(data.about);
+      setUserAvatar(data.avatar);
+    })
+    .catch(error=>{
+      console.log(error);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    Api.getInitialCards().then((data) => {
+      setCards(
+        data.map((item) => ({
+          cardID: item._id,
+          imageSrc: item.link,
+          imageAlt: item.name,
+          cardTitle: item.name,
+          cardLikes: item.likes.length,
+        }))
+      );
+    })
+    .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
@@ -12,7 +47,7 @@ function Main(props) {
         <section className="profile">
           <div className="profile__photo_container">
             <img
-              src={props.userAvatar}
+              src={userAvatar}
               className="profile__image"
               alt="Аватар пользователя"
             />
@@ -24,14 +59,14 @@ function Main(props) {
           </div>
           <div className="profile__info">
             <div className="profile__info-table">
-              <h1 className="profile__name">{props.name}</h1>
+              <h1 className="profile__name">{userName}</h1>
               <button
                 type="button"
                 className="profile__edit-icon"
                 onClick={props.onEditProfile}
               />
             </div>
-            <p className="profile__description">{props.description}</p>
+            <p className="profile__description">{userDescription}</p>
           </div>
           <div className="profile__rectangle">
             <button
@@ -44,7 +79,7 @@ function Main(props) {
           </div>
         </section>
         <section className="elements">
-          {props.allCards.map(({ cardID, ...props }) => (
+          {cards.map(({ cardID, ...props }) => (
             <Card onCardClick={handleClick} key={cardID} {...props} />
           ))}
         </section>
@@ -55,3 +90,63 @@ function Main(props) {
 }
 
 export default Main;
+
+
+/**Код для 11 проектной работы */
+// import React from 'react';
+// import Card from './Card';
+
+// function Main(props) {
+//   function handleClick(imageSrc, cardTitle) {
+//     props.handleCardClick(imageSrc, cardTitle);
+//   }
+
+//   return (
+//     <>
+//       <main className="content">
+//         <section className="profile">
+//           <div className="profile__photo_container">
+//             <img
+//               src={props.userAvatar}
+//               className="profile__image"
+//               alt="Аватар пользователя"
+//             />
+//             <button
+//               type="button"
+//               className="profile__editingAvatar-icon"
+//               onClick={props.onEditAvatar}
+//             ></button>
+//           </div>
+//           <div className="profile__info">
+//             <div className="profile__info-table">
+//               <h1 className="profile__name">{props.name}</h1>
+//               <button
+//                 type="button"
+//                 className="profile__edit-icon"
+//                 onClick={props.onEditProfile}
+//               />
+//             </div>
+//             <p className="profile__description">{props.description}</p>
+//           </div>
+//           <div className="profile__rectangle">
+//             <button
+//               type="button"
+//               className="profile__addingCard-icon"
+//               onClick={props.onAddPlace}
+//             >
+//               {" "}
+//             </button>
+//           </div>
+//         </section>
+//         <section className="elements">
+//           {props.allCards.map(({ cardID, ...props }) => (
+//             <Card onCardClick={handleClick} key={cardID} {...props} />
+//           ))}
+//         </section>
+//       </main>
+//       <div className="overlay" />
+//     </>
+//   );
+// }
+
+// export default Main;
